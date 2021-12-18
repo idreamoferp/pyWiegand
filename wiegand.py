639149c1a98b510fb7babf84d2a8ecf8f56b7eca
Justin Mangini <justin.mangini#esg.global>
2021-12-18 17:32:11 +0000

    update timeouts and card readmethods

--- wiegand.py
+++ wiegand.py
@@ -22,17 +22,24 @@ class Wiegand:
 
     def on_pin(self, is_one, newstate):
         now = time.time()
-        if self.last_bit_read is not None and now - self.last_bit_read < 2:
+        if self.last_bit_read is not None and now - self.last_bit_read < 0.0016:
             # too fast
             return
-
+        
+        if self.next_card:
+            if now - self.last_bit_read > 0.25:
+                self.next_card = 0
+                self._bits = 0
+        
         self.last_bit_read = now
         self.next_card <<= 1
         if is_one: self.next_card |= 1
         self._bits += 1
+        if self._bits == 26:
+            
+            self._cardcheck()
         
-        self._cardcheck()
-
+        
     def get_card(self):
         if self.last_card is None:
             return None
@@ -46,15 +53,14 @@ class Wiegand:
 
     def _cardcheck(self):
         if self.last_bit_read is None: return
-        now = time.time()
-        if now - self.last_bit_read > 50:
-            # too slow - new start!
-            self.last_bit_read = None
-            self.last_card = self.next_card
-            self.next_card = 0
-            self._bits = 0
-            self.cards_read += 1
-            self.callback(self.get_card(), self.get_facility_code(), self.cards_read)
+    
+        # too slow - new start!
+        self.last_bit_read = None
+        self.last_card = self.next_card
+        self.next_card = 0
+        self._bits = 0
+        self.cards_read += 1
+        self.callback(self.get_card(), self.get_facility_code(), self.cards_read)
 
 
 if __name__ == "__main__":
@@ -64,21 +70,24 @@ if __name__ == "__main__":
     w = Wiegand(callback=w_callback)
     
     import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BCM)  
+    GPIO.setmode(GPIO.BCM)
+    d0 = 26
+    d1 = 27
     
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(d0, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(d1, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
     
     def pin_change(x):
+        
         pin_1 = False
-        if x == 23:
+        if x == d1:
             pin_1 = True
             
         pin_value = GPIO.input(x)    
         w.on_pin(pin_1, pin_value)
     
-    GPIO.add_event_detect(23, GPIO.BOTH, callback=pin_change, bouncetime=100)  
-    GPIO.add_event_detect(17, GPIO.BOTH, callback=pin_change, bouncetime=100)  
+    GPIO.add_event_detect(d0, GPIO.FALLING, callback=pin_change)  
+    GPIO.add_event_detect(d1, GPIO.FALLING, callback=pin_change)  
     
     while 1:
         time.sleep(.1)
